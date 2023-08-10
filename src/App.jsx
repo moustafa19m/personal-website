e722ce49287d571e49af7077178284ed2d4d174e
Moustafa Makhlouf <moustafamakhlouf@Moustafas-MacBook-Pro.local>
2023-08-09 20:35:33 -0700

    changed favicon and site name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import myLogo from '/favicon.svg'
 import './App.css'
 
 function App() {
@@ -10,13 +10,13 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={myLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Moustafa Makhlouf's Website in React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
